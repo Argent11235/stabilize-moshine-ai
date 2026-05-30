679962085617d2d4246abbaf2c4df5b91524e189
Cain <154765484+Argent11235@users.noreply.github.com>
2026-05-30 10:16:23 -0400

    Update data.lua

--- data.lua
+++ data.lua
@@ -3,9 +3,5 @@ local shared = require("shared")
 local function remove_spoil_mechanic(prototype)
   prototype.spoil_ticks = nil
 
-  if mods["spoiling-plant"] == nil then
-    prototype.spoil_result = nil
-  end
-
   prototype.spoil_to_trigger_result = nil
 end
